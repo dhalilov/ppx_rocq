7d24089eb1e08abf5e47102a335a7b328f6786be
Dario Halilovic <dario.halilovic@epfl.ch>
2026-05-26 11:47:21 +0200

    feat: implement Ast_diagnostics.warn

--- src/ast_diagnostics.ml
+++ src/ast_diagnostics.ml
@@ -5,3 +5,10 @@ open Ppxlib
 let error ~loc format args =
   Ast_builder.Default.pexp_extension ~loc @@
     Location.error_extensionf ~loc format args
+
+let warn ~loc warning expr =
+  let warning = Ast_builder.Default.estring ~loc warning in
+  [%expr [%e expr] [@ocaml.ppwarning [%e warning]]]
+
+let warn' warnings expr =
+  List.fold_left (fun expr warning -> warn ~loc:warning.loc warning.txt expr) expr warnings
--- src/ast_diagnostics.mli
+++ src/ast_diagnostics.mli
@@ -4,3 +4,9 @@ open Ppxlib
 
 val error : loc:location -> ('a -> extension, Format.formatter, unit, extension) format4 -> 'a -> expression
 (** [error ~loc format args] creates an expression that embeds the error [format], formatted with the given [args]. *)
+
+val warn : loc:location -> string -> expression -> expression
+(** [warn ~loc warning expr] attaches the given warning message to [expr]. *)
+
+val warn' : string loc list -> expression -> expression
+(** [warn' warnings expr] attaches the given list of warnings to [expr]. *)
