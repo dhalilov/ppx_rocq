7f62b854007ca06f05f3131711d54630451f7894
Dario Halilovic <dario.halilovic@epfl.ch>
2026-06-26 11:03:05 +0200

    chore: simplify Pattern_matching

--- runtime/pattern_matching.ml
+++ runtime/pattern_matching.ml
@@ -1,68 +1,73 @@
 (** Runtime support for term and goal pattern matching. *)
 
+open Names
 open Tactics
+open Ltac2_plugin
+
+(** {1 General matching algorithms} *)
+
+let no_match_error = (CErrors.UserError Pp.(str "No matching clauses for match."), Exninfo.null)
+
+(** {2 Backtracking match} *)
+
+let rec one_match ~error = function
+  | [] ->
+     let (e, info) = error in
+     Proofview.tclZERO ~info e
+  | tac :: rest ->
+     try
+       Proofview.tclOR tac (fun error -> one_match ~error rest)
+     with Constr_matching.PatternMatchingFailure ->
+       one_match ~error rest
+
+let one_match cases = one_match ~error:no_match_error cases
+
+(** {1 Matching over terms} *)
 
 type 'a case = pattern Proofview.tactic * 'a continuation
 and pattern = Pattern.constr_pattern
 and 'a continuation = substitution -> 'a Proofview.tactic
 and substitution = Ltac_pretype.patvar_map
 
-let matching_error = (CErrors.UserError Pp.(str "No matching clauses for match."), Exninfo.null)
-
 let match_term t ~cases =
   let* env = Tactics.env in
   let* sigma = Tactics.evar_map in
-  let rec test_cases (e, info) = function
-    | [] -> Proofview.tclZERO ~info e
-    | (pattern, k) :: rest ->
-       let* pattern in
-       try
-         let subst = Constr_matching.matches env sigma pattern t in
-         let tac = k subst in
-         Proofview.tclOR tac (fun e -> test_cases e rest)
-       with Constr_matching.PatternMatchingFailure ->
-         test_cases (e, info) rest
+  let case_tactic (pattern, k) =
+    let* pattern in
+    let subst = Constr_matching.matches env sigma pattern t in
+    k subst
   in
-  test_cases matching_error cases
+  one_match (List.map case_tactic cases)
 
 let match_term' t ~cases =
   let* t in match_term t ~cases
 
-type 'a goal_case = goal_pattern Proofview.tactic * (Names.Id.t array -> 'a continuation)
+(** {1 Matching over goals} *)
+
+type 'a goal_case = goal_pattern Proofview.tactic * (Id.t array -> 'a continuation)
 and goal_pattern =
   { hypotheses: (pattern option * pattern) list;
     conclusion: pattern }
 
-open Ltac2_plugin
-
 let compile_case case =
   let* case in
   let open Tac2match in
-  let hyp_to_ltac2 (binder, typ) =
-    let binder =
-      match binder with
-      | Some pattern -> Some (MatchPattern pattern)
-      | None -> None
-    in binder, MatchPattern typ
+  let binder_to_pattern = function
+    | Some pattern -> Some (MatchPattern pattern)
+    | None -> None
   in
-  let hypotheses = List.map hyp_to_ltac2 case.hypotheses in
+  let to_patterns (binder, typ) = binder_to_pattern binder, MatchPattern typ in
+  let hypotheses = List.map to_patterns case.hypotheses in
   let conclusion = MatchPattern case.conclusion in
   Proofview.tclUNIT (hypotheses, conclusion)
 
-let match_goal ?(reverse = false) t ~cases =
+let match_goal ?(reverse = false) goal ~cases =
   let* env = Tactics.env in
   let* sigma = Tactics.evar_map in
-  let cases = List.map (fun (case, k) -> compile_case case, k) cases in
-  let rec test_cases (e, info) = function
-    | [] -> Proofview.tclZERO ~info e
-    | (rule, k) :: rest ->
-       let* rule in
-       try
-         let* (hypotheses, context, subst) = Ltac2_plugin.Tac2match.match_goal env sigma t ~rev:reverse rule in
-         let hyp_names = Array.of_list @@ List.map (fun (name, _, _) -> name) hypotheses in
-         let tac = k hyp_names subst in
-         Proofview.tclOR tac (fun e -> test_cases e rest)
-       with Constr_matching.PatternMatchingFailure ->
-         test_cases (e, info) rest
+  let case_tactic (case, k) =
+    let* rule = compile_case case in
+    let* (hypotheses, context, subst) = Tac2match.match_goal env sigma goal ~rev:reverse rule in
+    let hyp_names = Array.of_list @@ List.map (fun (name, _, _) -> name) hypotheses in
+    k hyp_names subst
   in
-  test_cases matching_error cases
+  one_match (List.map case_tactic cases)
